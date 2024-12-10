26ce8343877de3f25079384cfa43be96ddef6409
songruining <kevinsong@lilith.com>
2024-12-10 18:38:55 +0800

    refactor: simplified Android testing project

--- tests/android/app/src/main/java/com/example/test_luau_debugger/MainActivity.kt
+++ tests/android/app/src/main/java/com/example/test_luau_debugger/MainActivity.kt
@@ -9,51 +9,34 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.test_luau_debugger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val nativeLibraryDir = this.applicationInfo.nativeLibraryDir
+        System.loadLibrary("luaud")
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        binding.button.setOnClickListener {
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            run {
+                val toast = Toast.makeText(
+                    this,
+                    "launching luaud",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
+                this.main(3, arrayOf("luaud", "58000", "/sdcard/luau-debugger/tests/main.lua"))
+            }
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 
     external fun main(argc: Int, argv: Array<String>): Int
